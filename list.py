2a7e2b081ad000a41854821d5bc180a0f87184bc
EndL11 <podobailo.andriy@gmail.com>
2020-09-08 15:11:00 +0300

    done simple list task

---
+++ list.py
@@ -0,0 +1,6 @@
+# create list with different types
+# create new list based on previous from 1 to 3 item and print it
+
+some_list = [1, 'hello', 'python', 7.23, [1, 2, 3, 4, 5]]
+new_list = some_list[:3]
+print(new_list)
