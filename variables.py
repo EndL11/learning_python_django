339844ac99d420b4fb77b77b80932e1cb54319aa
EndL11 <podobailo.andriy@gmail.com>
2020-09-08 12:04:04 +0300

    done task of variables and simple math operations

---
+++ variables.py
@@ -0,0 +1,28 @@
+# first task
+# describe car properties and change it under initialization
+
+# car_color = "red"
+# car_model = "audi a6"
+# car_year = 2013
+# car_count_of_doors = 4
+#
+# print(car_color)
+# print(car_model)
+# print(car_year)
+# print(car_count_of_doors)
+#
+# car_color = "blue"
+# print(car_color)
+
+
+# second task
+# calculate average age of five people
+andrew_age = 20
+boris_age = 25
+roman_age = 32
+john_age = 16
+jason_age = 22
+numbers_of_people = 5
+
+average_age = (andrew_age + boris_age + roman_age + john_age + jason_age) // numbers_of_people
+print(average_age)
